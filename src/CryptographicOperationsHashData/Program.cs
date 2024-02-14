6a3ae985ef1d4d5bec569c520fb29d6765225b45
Nelson Nobre <technobre@gmail.com>
2024-02-14 21:18:52 +0000

    Update documentation

--- src/CryptographicOperationsHashData/Program.cs
+++ src/CryptographicOperationsHashData/Program.cs
@@ -2,5 +2,5 @@
 using System.Text;
 
 var bytes = Encoding.ASCII.GetBytes("Hello .NET 9!");
-var hash = CryptographicOperations.HashData(HashAlgorithmName.SHA512, bytes);
+var hash = CryptographicOperations.HashData(HashAlgorithmName.SHA512, bytes); // `CryptographicOperations.HashData` is new in .NET 9
 Console.WriteLine($"SHA512: {BitConverter.ToString(hash).Replace("-", "")}");
--- src/KMACAlgorithms/Program.cs
+++ src/KMACAlgorithms/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Security.Cryptography;
 
-Console.WriteLine($"Kmac128 is Supported algorithms: {Kmac128.IsSupported}");
-Console.WriteLine($"Kmac256 is Supported algorithms: {Kmac256.IsSupported}");
-Console.WriteLine($"KmacXof128 is Supported algorithms: {KmacXof128.IsSupported}");
-Console.WriteLine($"KmacXof256 is Supported algorithms: {KmacXof256.IsSupported}");
+Console.WriteLine($"Kmac128 is Supported algorithms: {Kmac128.IsSupported}"); // `Kmac128` is new in .NET 9
+Console.WriteLine($"Kmac256 is Supported algorithms: {Kmac256.IsSupported}"); // `Kmac256` is new in .NET 9
+Console.WriteLine($"KmacXof128 is Supported algorithms: {KmacXof128.IsSupported}"); // `KmacXof128 is new in .NET 9
+Console.WriteLine($"KmacXof256 is Supported algorithms: {KmacXof256.IsSupported}"); // `KmacXof256 is new in .NET 9
